7248af4c270758e0c7036675198fccafea5e0095
Jonathan Uttankar <jonathanuttankar@Jonathans-MacBook-Air.local>
2017-06-29 18:46:21 +0530

    Completed email authentication with firebase

--- Click/SignInVC.swift
+++ Click/SignInVC.swift
@@ -13,6 +13,11 @@ import Firebase
 
 class SignInVC: UIViewController {
 
+    
+    @IBOutlet weak var emailField: FancyField!
+    @IBOutlet weak var passwordField: FancyField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -50,5 +55,24 @@ class SignInVC: UIViewController {
         }
     }
 
+    @IBAction func signInTapped(_ sender: Any) {
+        
+        if let email = emailField.text, let pwd = passwordField.text {
+            FIRAuth.auth()?.signIn(withEmail: email, password: pwd, completion: { (user, error) in
+                if error == nil {
+                    print("Jonny: Email User Authenticated with Firebase")
+                } else {
+                    FIRAuth.auth()?.createUser(withEmail: email, password: pwd, completion: { (user, error) in
+                        if error != nil {
+                            print("Jonny: Unable to authenticate with firebase")
+                        } else {
+                            print("Jonny: Successfully authenticated with firebase")
+                        }
+                    })
+                }
+            })
+        }
+        
+    }
 }
 
