adf770671cf7cc38e8cc5904ddc87200610a8f32
Jonathan Uttankar <jonathanuttankar@Jonathans-MacBook-Air.local>
2017-07-03 08:39:47 +0530

    Feed UI

---
+++ Click/CircleView.swift
@@ -0,0 +1,24 @@
+//
+//  CircleView.swift
+//  Click
+//
+//  Created by Jonathan Uttankar on 01/07/17.
+//  Copyright © 2017 Jonathan Uttankar. All rights reserved.
+//
+
+import UIKit
+
+class CircleView: UIImageView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        layer.cornerRadius = self.frame.width / 2
+    }
+
+}
