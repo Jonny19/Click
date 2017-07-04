3c96dff4627ccf1e1bc542407d94921b0875d79a
Jonathan Uttankar <jonathanuttankar@Jonathans-MacBook-Air.local>
2017-07-04 16:06:10 +0530

    Firebase data display

--- Click/FeedVC.swift
+++ Click/FeedVC.swift
@@ -48,9 +48,15 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let post = posts[indexPath.row]
-        print("Jonny: \(post)")
-        return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as? PostCell {
+            cell.configureCell(post: post)
+            return cell
+        } else {
+            return PostCell()
+        }
     }
 
     @IBAction func signOutTapped(_ sender: Any) {
